f0c75f72e65f848972f11097735228f94fc0018d
nsalman <nsalman@attentigroup.com>
2023-03-31 12:09:51 +0300

    Update provision - install docker compose

--- provision-infra/provision-infra.sh
+++ provision-infra/provision-infra.sh
@@ -3,14 +3,19 @@ set -x
 
 
 
-
+# extract key for authenticate 
 cd ../dev-infra
 export VM_IP=$(terraform output public_ip_address)
 cd ../provision-infra
 ls -la
+
+
 source .env
 printf "\n Dev VM IP : %s\n user : %s\n private key full path : %s \n\n" $VM_IP $DEVOPS_USER $PRIVATE_KEY_PATH
 chmod 600 $PRIVATE_KEY_PATH
+
+# test connection
 ansible -m ping dev -vvv -i inventory.yaml 
+
+# Provition 
 ansible-playbook -vvv -i inventory.yaml provision-infrastructure-playbook.yaml
-# ansible-playbook  -i inventory.yaml --user=$DEVOPS_USER provision-infrastructure-playbook.yaml
